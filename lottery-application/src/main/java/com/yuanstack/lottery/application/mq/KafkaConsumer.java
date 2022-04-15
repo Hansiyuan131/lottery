e3402c5d0a5da9fc0b69e1c17bed19ef5239994e
Hansiyuan131 <422295068@qq.com>
2022-04-15 11:22:13 +0800

    yuan,feat: 集成Kafka消息队列

---
+++ lottery-application/src/main/java/com/yuanstack/lottery/application/mq/KafkaConsumer.java
@@ -0,0 +1,34 @@
+package com.yuanstack.lottery.application.mq;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.Acknowledgment;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+/**
+ * @description: 消息消费者
+ * @author: hansiyuan
+ * @date: 2022/4/15 11:19 AM
+ */
+@Component
+@Slf4j
+public class KafkaConsumer {
+
+    @KafkaListener(topics = KafkaProducer.TOPIC_TEST, groupId = KafkaProducer.TOPIC_GROUP)
+    public void topicTest(ConsumerRecord<?, ?> record, Acknowledgment ack, @Header(KafkaHeaders.RECEIVED_TOPIC) String topic) {
+        Optional<?> message = Optional.ofNullable(record.value());
+        if (message.isPresent()) {
+            Object msg = message.get();
+            log.info("topic_test 消费了： Topic:" + topic + ",Message:" + msg);
+            ack.acknowledge();
+        }
+    }
+
+}
